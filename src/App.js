6784c9ac1e6fef479ce10222ab62599f2f4f24c7
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-07 22:46:24 -0400

    Add sidebar elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <main className="layout">
+      <section className="sidebar">
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu"></nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
+      </section>
+      
+      <section className="schedule">
+        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+      </section>
+  </main>
   );
 }
 
