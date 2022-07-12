a32e0c7faffc2d6738ca577ef2bfd82acb3a195f
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-11 22:12:10 -0400

    Add button onclick and disabled status

--- src/components/Button.js
+++ src/components/Button.js
@@ -12,7 +12,11 @@ export default function Button(props) {
   }
 
   return (
-    <button className={buttonClass}>
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
       {props.children}
     </button>
   )
