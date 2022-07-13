e01ee88d62cb3480e04dc53aa56117d3f72111d4
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-12 21:18:56 -0400

    Add DayListItem stories

---
+++ src/stories/DayListItem.stories.js
@@ -0,0 +1,30 @@
+import { action } from "@storybook/addon-actions";
+
+import "../index.scss";
+import DayListItem from "../components/DayListItem";
+
+export default {
+  title: 'DayListItem',
+  component: DayListItem,
+  parameters: {
+    backgrounds: {
+      values: [{ name: "dark", value: "#222f3e" }]
+    } 
+  }
+};
+
+export const Unselected = () => {
+  return <DayListItem name="Monday" spots={5} />
+}
+
+export const Selected = () => {
+  return <DayListItem name="Monday" spots={5} selected />
+}
+
+export const Full = () => {
+  return <DayListItem name="Monday" spots={0} />
+}
+
+export const Clickable = () => {
+  return <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} />
+}
