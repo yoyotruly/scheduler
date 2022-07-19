2da03a9365c23a09caaf3107d7012a91cab65cbb
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-18 21:44:39 -0400

    Add appointment show component

--- src/stories/Appointment.stories.js
+++ src/stories/Appointment.stories.js
@@ -4,6 +4,7 @@ import "../index.scss";
 import Appointment from "../components/Appointment";
 import Header from "../components/Appointment/Header";
 import Empty from "../components/Appointment/Empty";
+import Show from "../components/Appointment/Show";
 
 export default {
   title: 'Appointment',
@@ -23,10 +24,27 @@ export const AppointmentWithTime = () => {
   return <Appointment time="12pm" />
 }
 
-export const HeaderDefault = () => {
+export const AppointmentHeader = () => {
   return <Header time="12pm" />
 }
 
-export const EmptyDefault = () => {
+export const AppointmentEmpty = () => {
   return <Empty onAdd={action("onAdd")} />
+}
+
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png"
+};
+
+export const AppointmentShow = () => {
+  return (
+    <Show
+      student="Lydia Miller-Jones"
+      interviewer={interviewer}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+    />
+  )
 }
