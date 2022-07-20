19bc48b58de60a1484ebc5b957032b902f4d6cda
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-19 21:20:23 -0400

    Fix confirm onClick value

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -38,7 +38,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={}>Save</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
     </main>
