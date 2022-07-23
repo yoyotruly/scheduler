ad203cbf20d3f208b1165d0b7f3c39eadc358d0d
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-23 18:26:01 -0400

    Refactor app to use transformed interview data

--- src/App.js
+++ src/App.js
@@ -3,73 +3,50 @@ import axios from "axios";
 import DayList from "./components/DayList";
 import Appointment from "./components/Appointment";
 
+import { getAppointmentsForDay, getInterview } from "./helpers/selectors";
+
 import "./App.scss";
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
 
 function App() {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const setDay = day => {
     return setState(prevState => ({ ...prevState, day }))
   };
 
-  const setDays = days => {
-    return setState(prevState => ({ ...prevState, days }))
-  }
-
   useEffect(() => {
-    axios
-    .get("/api/days")
-    .then(response => setDays(response.data))
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then((all) => {
+      setState(prevState => {
+        return {
+          ...prevState,
+          days: all[0].data,
+          appointments: all[1].data,
+          interviewers: all[2].data
+        }
+      })
+    });
   }, [])
 
-  const appointmentElements = Object.values(appointments).map(appointment => {
+  const dailyAppointments = getAppointmentsForDay(state, state.day)
+
+  const appointmentElements = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+
     return (
       <Appointment
         key={appointment.id}
-        {...appointment} 
+        {...appointment}
+        interview={interview}
       />
     )
   })
