cf026b8fbea04490ba28dd639d424af8293ce597
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-23 18:23:49 -0400

    Add getInterviewer helper function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -7,3 +7,12 @@ export function getAppointmentsForDay(state, day) {
     return state.appointments[appointment]
   })
 }
+
+export function getInterview(state, interview) {
+  if (!interview) return null
+  
+  return {
+    ...interview,
+    interviewer: state.interviewers[interview.interviewer]
+  }
+}
