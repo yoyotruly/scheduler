5f1de72025c78d9603cb035c3cd53a047a329957
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-23 23:07:49 -0400

    Add appointment state transition

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,15 +1,37 @@
+import useVisualMode from "../../hooks/useVisualMode";
 import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
+import Form from "./Form";
 import "./styles.scss";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ?
-        <Show student={props.interview.student} interviewer={props.interview.interviewer} /> :
-        <Empty />}
+      { mode === "SHOW" &&
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      }
+      { mode === "EMPTY" &&
+        <Empty onAdd={() => transition(CREATE)} />
+      }
+      { mode === "CREATE" &&
+        <Form
+          interviewers={[]}
+          onCancel={back}
+        />
+      }
     </article>
   )
 }
