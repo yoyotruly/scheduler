f84bb6de735f530d8435f3df363d7cbbd05a8c66
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-23 22:51:27 -0400

    Add custom hook for visual mode transition

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (nextMode, replace = false) => {
+    setMode(nextMode);
+
+    if (!replace) {
+      setHistory(prevHistory => [...prevHistory, nextMode]);
+    }
+  }
+
+  const back = () => {
+    if (history.length > 1) {
+      setMode(history[history.length - 2])
+      setHistory(prevHistory => [...prevHistory.slice(0, -1)])
+    }
+  }
+
+  return { mode, transition, back }
+}
