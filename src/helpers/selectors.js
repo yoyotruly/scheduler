16752b0e9a90e8793822d0d593a50f886c256847
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-23 23:19:40 -0400

    Add getInterviewersByDay helper function and tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -16,3 +16,14 @@ export function getInterview(state, interview) {
     interviewer: state.interviewers[interview.interviewer]
   }
 }
+
+export function getInterviewersForDay(state, day) {
+  const filteredDay = state.days.filter(dayObj => dayObj.name === day)
+
+  if (!filteredDay.length) return [];
+  
+  return filteredDay[0].interviewers.map(interviewer => {
+    return state.interviewers[interviewer]
+  })
+
+}
--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -1,16 +1,18 @@
-import { getAppointmentsForDay, getInterview } from "./selectors";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "./selectors";
 
 const state = {
   days: [
     {
       id: 1,
       name: "Monday",
-      appointments: [1, 2, 3]
+      appointments: [1, 2, 3],
+      interviewers: [1, 3, 4, 6]
     },
     {
       id: 2,
       name: "Tuesday",
-      appointments: [4, 5]
+      appointments: [4, 5],
+      interviewers: [2, 5]
     }
   ],
   appointments: {
@@ -38,6 +40,26 @@ const state = {
       id: 2,
       name: "Tori Malcolm",
       avatar: "https://i.imgur.com/Nmx0Qxo.png"
+    },
+    "3": {
+      id: 3,
+      name: "Mildred Nazir",
+      avatar: "https://i.imgur.com/T2WwVfS.png"
+    },
+    "4": {
+      id: 4,
+      name: "Cohana Roy",
+      avatar: "https://i.imgur.com/FK8V841.jpg"
+    },
+    "5": {
+      id: 5,
+      name: "Sven Jones",
+      avatar: "https://i.imgur.com/twYrpay.jpg"
+    },
+    "6": {
+      id: 6,
+      name: "Susan Reynolds",
+      avatar: "https://i.imgur.com/TdOAdde.jpg"
     }
   }
 };
@@ -89,4 +111,32 @@ test("getInterview returns an object with the interviewer data", () => {
 test("getInterview returns null if no interview is booked", () => {
   const result = getInterview(state, state.appointments["2"].interview);
   expect(result).toBeNull();
+});
+
+/* ------ tests for getInterviewersForDay ------ */
+
+test("getInterviewersForDay returns an array", () => {
+  const result = getInterviewersForDay(state, "Monday");
+  expect(Array.isArray(result)).toBe(true);
+});
+
+test("getInterviewersForDay returns an array with a length matching the number of interviewers for that day", () => {
+  const result = getInterviewersForDay(state, "Monday");
+  expect(result.length).toEqual(4);
+});
+
+test("getInterviewersForDay returns an array containing the correct interviewer objects", () => {
+  const [first, second] = getInterviewersForDay(state, "Tuesday");
+  expect(first).toEqual(state.interviewers["2"]);
+  expect(second).toEqual(state.interviewers["5"]);
+});
+
+test("getInterviewersForDay returns an empty array when the days data is empty", () => {
+  const result = getInterviewersForDay({ days: [] }, "Monday");
+  expect(result.length).toEqual(0);
+});
+
+test("getInterviewersForDay returns an empty array when the day is not found", () => {
+  const result = getInterviewersForDay(state, "Wednesday");
+  expect(result.length).toEqual(0);
 });
