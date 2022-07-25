02bab73be04feb67c76994ab8eba68b4fdb9d09c
Yoyo Yang <yoyo.truly@gmail.com>
2022-07-24 20:35:38 -0400

    Add Edit and Confirm status

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,14 +3,17 @@ import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
 import Status from "./Status";
+import Confirm from "./Confirm";
 import Form from "./Form";
 import "./styles.scss";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const EDIT = "EDIT";
 const SAVING = "SAVING";
 const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
@@ -43,7 +46,8 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-          onDelete={remove}
+          onEdit={() => transition(EDIT)}
+          onDelete={() => transition(CONFIRM)}
         />
       }
       { mode === "EMPTY" && <Empty onAdd={() => transition(CREATE)} /> }
@@ -54,8 +58,24 @@ export default function Appointment(props) {
           onSave={save}
         />
       }
+      { mode === "EDIT" &&
+        <Form
+          interviewers={props.interviewers}
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.id}
+          onCancel={back}
+          onSave={save}
+        />
+      }
       { mode === "SAVING" && <Status message="Saving..." /> }
       { mode === "DELETING" && <Status message="DELETING..." /> }
+      { mode === "CONFIRM" &&
+        <Confirm
+          message="Are you sure you want to delete the appointment?"
+          onConfirm={remove}
+          onCancel={back}
+        />
+      }
     </article>
   )
 }
