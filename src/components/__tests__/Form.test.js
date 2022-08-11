395a4b35fc32627089817a9875692bac434602f5
Yoyo Yang <yoyo.truly@gmail.com>
2022-08-10 21:02:48 -0400

    Add more tests for Form component

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -57,22 +57,58 @@ describe("Form", () => {
     expect(onSave).not.toHaveBeenCalled();
   });
 
-  it("calls onSave function when the name is defined", () => {
+  it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
     render(
       <Form
         interviewers={interviewers}
-        student="Lydia Miller-Jones"
-        interviewer={interviewers[0].id}
         onSave={onSave}
+        interviewer={interviewers[0].id}
       />
     );
+
+    fireEvent.click(screen.getByText("Save"));
+
+    expect(
+      screen.getByText(/student name cannot be blank/i)
+    ).toBeInTheDocument();
+    expect(onSave).not.toHaveBeenCalled();
+
+    fireEvent.change(screen.getByPlaceholderText("Enter Student Name"), {
+      target: { value: "Lydia Miller-Jones" },
+    });
+
     fireEvent.click(screen.getByText("Save"));
 
     expect(screen.queryByText(/student name cannot be blank/i)).toBeNull();
-    expect(screen.queryByText(/please select an interviewer/i)).toBeNull();
 
     expect(onSave).toHaveBeenCalledTimes(1);
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
+  });
+
+  it("calls onCancel and resets the input field", () => {
+    const onCancel = jest.fn();
+    render(
+      <Form
+        interviewers={interviewers}
+        student="Lydia Mill-Jones"
+        onSave={jest.fn()}
+        onCancel={onCancel}
+      />
+    );
+
+    fireEvent.click(screen.getByText("Save"));
+
+    fireEvent.change(screen.getByPlaceholderText("Enter Student Name"), {
+      target: { value: "Lydia Miller-Jones" },
+    });
+
+    fireEvent.click(screen.getByText("Cancel"));
+
+    expect(screen.queryByText(/student name cannot be blank/i)).toBeNull();
+
+    expect(screen.getByPlaceholderText("Enter Student Name")).toHaveValue("");
+
+    expect(onCancel).toHaveBeenCalledTimes(1);
   });
 });
