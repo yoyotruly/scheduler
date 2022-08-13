6af3aa9035393de527f379406ceffac6a0e01115
Yoyo Yang <yoyo.truly@gmail.com>
2022-08-13 12:25:54 -0400

    Add mock test server

---
+++ src/mocks/testServer.js
@@ -0,0 +1,81 @@
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+
+const fixtures = {
+  days: [
+    {
+      id: 1,
+      name: "Monday",
+      appointments: [1, 2],
+      interviewers: [1, 2],
+      spots: 1,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      appointments: [3, 4],
+      interviewers: [3, 4],
+      spots: 1,
+    },
+  ],
+  appointments: {
+    1: { id: 1, time: "12pm", interview: null },
+    2: {
+      id: 2,
+      time: "1pm",
+      interview: { student: "Archie Cohen", interviewer: 2 },
+    },
+    3: {
+      id: 3,
+      time: "2pm",
+      interview: { student: "Leopold Silvers", interviewer: 4 },
+    },
+    4: { id: 4, time: "3pm", interview: null },
+  },
+  interviewers: {
+    1: {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png",
+    },
+    2: {
+      id: 2,
+      name: "Tori Malcolm",
+      avatar: "https://i.imgur.com/Nmx0Qxo.png",
+    },
+    3: {
+      id: 3,
+      name: "Mildred Nazir",
+      avatar: "https://i.imgur.com/T2WwVfS.png",
+    },
+    4: {
+      id: 4,
+      name: "Cohana Roy",
+      avatar: "https://i.imgur.com/FK8V841.jpg",
+    },
+  },
+};
+
+const server = setupServer(
+  rest.get("/api/days", async (req, res, ctx) => {
+    return res(ctx.json(fixtures.days));
+  }),
+
+  rest.get("/api/appointments", async (req, res, ctx) => {
+    return res(ctx.json(fixtures.appointments));
+  }),
+
+  rest.get("/api/interviewers", async (req, res, ctx) => {
+    return res(ctx.json(fixtures.interviewers));
+  }),
+
+  rest.put("/api/appointments/1", async (req, res, ctx) => {
+    return res(ctx.json());
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+export { server, rest };
