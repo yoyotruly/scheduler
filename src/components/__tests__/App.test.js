96b5bbf7563887fb92b1b46bfb60f2572018987a
Yoyo Yang <yoyo.truly@gmail.com>
2022-08-14 00:28:09 -0400

    Add integration test for App component

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,96 +1,177 @@
-import axios from "axios";
-import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+import {
+  render,
+  fireEvent,
+  screen,
+  getByAltText,
+  getAllByTestId,
+  findByText,
+  getByPlaceholderText,
+  getByText,
+  queryByText,
+  findByAltText,
+} from "@testing-library/react";
+import { server, rest } from "../../mocks/testServer";
 
 import App from "../../App";
 
-const fixtures = {
-  days: [
-    {
-      id: 1,
-      name: "Monday",
-      appointments: [1, 2],
-      interviewers: [1, 2],
-      spots: 1,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      appointments: [3, 4],
-      interviewers: [3, 4],
-      spots: 1,
-    },
-  ],
-  appointments: {
-    1: { id: 1, time: "12pm", interview: null },
-    2: {
-      id: 2,
-      time: "1pm",
-      interview: { student: "Archie Cohen", interviewer: 2 },
-    },
-    3: {
-      id: 3,
-      time: "2pm",
-      interview: { student: "Leopold Silvers", interviewer: 4 },
-    },
-    4: { id: 4, time: "3pm", interview: null },
-  },
-  interviewers: {
-    1: {
-      id: 1,
-      name: "Sylvia Palmer",
-      avatar: "https://i.imgur.com/LpaY82x.png",
-    },
-    2: {
-      id: 2,
-      name: "Tori Malcolm",
-      avatar: "https://i.imgur.com/Nmx0Qxo.png",
-    },
-    3: {
-      id: 3,
-      name: "Mildred Nazir",
-      avatar: "https://i.imgur.com/T2WwVfS.png",
-    },
-    4: {
-      id: 4,
-      name: "Cohana Roy",
-      avatar: "https://i.imgur.com/FK8V841.jpg",
-    },
-  },
-};
-
 describe("App", () => {
-  beforeEach(() => {
-    jest.spyOn(axios, "get").mockImplementation((url) => {
-      if (url === "/api/days") {
-        return Promise.resolve({
-          data: fixtures.days,
-        });
-      }
-
-      if (url === "/api/appointments") {
-        return Promise.resolve({
-          data: fixtures.appointments,
-        });
-      }
-
-      if (url === "/api/interviewers") {
-        return Promise.resolve({
-          data: fixtures.interviewers,
-        });
-      }
-    });
-  });
-
-  afterEach(() => {
-    axios.get.mockRestore();
-  });
-
   it("defaults to Monday and changes the schedule when a new day is selected", async () => {
-    await render(<App />);
+    render(<App />);
 
     await screen.findByText("Monday");
 
     fireEvent.click(screen.getByText("Tuesday"));
+
     expect(screen.getByText("Leopold Silvers")).toBeInTheDocument();
   });
+
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { container } = render(<App />);
+
+    await findByText(container, "Archie Cohen");
+
+    const appointment = getAllByTestId(container, "appointment")[0];
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" },
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    fireEvent.click(getByText(appointment, "Save"));
+
+    expect(getByText(appointment, /saving/i)).toBeInTheDocument();
+
+    await findByText(appointment, "Lydia Miller-Jones");
+
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+
+    expect(getByText(day, /no spots remaining/i)).toBeInTheDocument();
+  });
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    const { container } = render(<App />);
+
+    await findByText(container, "Archie Cohen");
+
+    const appointment = getAllByTestId(container, "appointment").find(
+      (appointment) => queryByText(appointment, "Archie Cohen")
+    );
+    fireEvent.click(getByAltText(appointment, "Delete"));
+
+    expect(
+      getByText(appointment, /delete the appointment/i)
+    ).toBeInTheDocument();
+
+    fireEvent.click(getByText(appointment, "Confirm"));
+
+    expect(getByText(appointment, /deleting/i)).toBeInTheDocument();
+
+    await findByAltText(appointment, "Add");
+
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+
+    expect(getByText(day, /2 spots remaining/i)).toBeInTheDocument();
+  });
+
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    const { container } = render(<App />);
+
+    await findByText(container, "Archie Cohen");
+
+    const appointment = getAllByTestId(container, "appointment").find(
+      (appointment) => queryByText(appointment, "Archie Cohen")
+    );
+    fireEvent.click(getByAltText(appointment, "Edit"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Rick Sanchez" },
+    });
+    fireEvent.click(getByAltText(appointment, "Tori Malcolm"));
+    fireEvent.click(getByText(appointment, "Save"));
+
+    expect(getByText(appointment, /saving/i)).toBeInTheDocument();
+
+    await findByText(appointment, "Rick Sanchez");
+    await findByText(appointment, "Tori Malcolm");
+
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+
+    expect(getByText(day, /1 spot remaining/i)).toBeInTheDocument();
+  });
+
+  // it("shows the save error when failing to save an appointment", async () => {
+  //   server.use(rest.put("/api/appointments/1"), (req, res, ctx) => {
+  //     return res(ctx.json({}));
+  //   });
+
+  //   const { container } = render(<App />);
+
+  //   await findByText(container, "Archie Cohen");
+
+  //   const appointment = getAllByTestId(container, "appointment")[0];
+  //   fireEvent.click(getByAltText(appointment, "Add"));
+
+  //   fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+  //     target: { value: "Lydia Miller-Jones" },
+  //   });
+  //   fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+  //   fireEvent.click(getByText(appointment, "Save"));
+
+  //   expect(getByText(appointment, /saving/i)).toBeInTheDocument();
+
+  //   await findByText(appointment, /could not save appointment/i);
+
+  //   fireEvent.click(getByAltText(appointment, "Close"));
+
+  //   expect(
+  //     getByPlaceholderText(appointment, /enter student name/i)
+  //   ).toBeInTheDocument();
+
+  //   const day = getAllByTestId(container, "day").find((day) =>
+  //     queryByText(day, "Monday")
+  //   );
+
+  //   expect(getByText(day, /1 spot remaining/i)).toBeInTheDocument();
+  // });
+
+  // it("shows the delete error when failing to delete an existing appointment", async () => {
+  //   server.use(rest.delete("/api/appointments/2"), (req, res, ctx) => {
+  //     return res(ctx.status(500));
+  //   });
+
+  //   const { container } = render(<App />);
+
+  //   await findByText(container, "Archie Cohen");
+
+  //   const appointment = getAllByTestId(container, "appointment").find(
+  //     (appointment) => queryByText(appointment, "Archie Cohen")
+  //   );
+  //   fireEvent.click(getByAltText(appointment, "Delete"));
+
+  //   expect(
+  //     getByText(appointment, /delete the appointment/i)
+  //   ).toBeInTheDocument();
+
+  //   fireEvent.click(getByText(appointment, "Confirm"));
+
+  //   expect(getByText(appointment, /deleting/i)).toBeInTheDocument();
+
+  //   await findByText(appointment, /could not delete appointment/i);
+
+  //   fireEvent.click(getByAltText(appointment, "Close"));
+
+  //   expect(getByText(appointment, /Archie Cohen/i)).toBeInTheDocument();
+
+  //   const day = getAllByTestId(container, "day").find((day) =>
+  //     queryByText(day, "Monday")
+  //   );
+
+  //   expect(getByText(day, /1 spot remaining/i)).toBeInTheDocument();
+  // });
 });
